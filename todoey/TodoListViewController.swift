efbb936f2d74512c47ad558aac36632f9681e0b7
Tài Nguyễn Đức <nguyenductai@tai-dep-trai.local>
2018-02-25 16:18:22 +0700

    First Basic

---
+++ todoey/TodoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  todoey
+//
+//  Created by Nguyen Duc Tai on 2/23/18.
+//  Copyright © 2018 Nguyen Duc Tai. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    var item:Array = ["mua trung","mua thit ga","mua thit bo"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return item.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = item[indexPath.row]
+        return cell
+    }
+    //Mark: tableview
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
